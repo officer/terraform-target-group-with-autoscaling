aff8860d7818e66f71da813d80954bc4ed5a3f6d
officer <kasaiso@amazon.com>
2019-09-05 12:31:41 +0100

    fix minor bug

--- main.tf
+++ main.tf
@@ -32,6 +32,6 @@ resource "aws_autoscaling_group" "autoscaling_group" {
       "ClosestToNextInstanceHour"
   ]
 
-  tags                  = ["${var.tags}"]
+  tags                   = ["${var.tags}"]
 
 }
